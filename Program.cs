5e5874a595cbb66ccdcefaa8f5fdebcae41d54e5
muslumozer <muslumozer1@gmail.com>
2022-10-23 21:23:49 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PuppeteerSharp;
+
+
+using var browserFetcher = new BrowserFetcher();
+await browserFetcher.DownloadAsync(BrowserFetcher.DefaultRevision);
+var browser = await Puppeteer.LaunchAsync(new LaunchOptions
+{
+    Headless = false
+});
+var page = await browser.NewPageAsync();
+await page.GoToAsync("https://www.google.com/");
+await page.ScreenshotAsync("image.jpg");
+
+Console.WriteLine("Hello, World!");
